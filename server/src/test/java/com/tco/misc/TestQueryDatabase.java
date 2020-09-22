80c44e306bde1950a089167f29f8c2f18fb25388
edgarv24 <edvarela@rams.colostate.edu>
2020-09-22 17:20:56 -0600

    Create QueryDatabase class to connect to database. Fixed minor issues and cleaned code a bit. Related #108

--- server/src/test/java/com/tco/misc/TestQueryDatabase.java
+++ server/src/test/java/com/tco/misc/TestQueryDatabase.java
@@ -15,6 +15,11 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestQueryDatabase {
     Connection conn;
     private QueryDatabase db;
+    String isTravis = System.getenv("TRAVIS");
+    private final String useTunnel = System.getenv("CS314_USE_DATABASE_TUNNEL");
+    private String expectedUserName;
+    private String expectedPassword;
+    private String expectedURL;
 
     @BeforeEach
     public void createConfigurationForTestCases() throws SQLException {
@@ -30,24 +35,27 @@ public class TestQueryDatabase {
     }
 
     @Test
-    @DisplayName("DB_URL should be jdbc:mysql://127.0.0.1:56247/cs314")
+    @DisplayName("DB_URL will depend on environment")
     public void testGetDB_URL() {
+        checkIfTravis();
         String url = db.getDbUrl();
-        assertEquals("jdbc:mysql://127.0.0.1:56247/cs314", url);
+        assertEquals(expectedURL, url);
     }
 
     @Test
-    @DisplayName("DB_User should be cs314-db")
+    @DisplayName("DB_User should be cs314-db or root")
     public void testGetDB_User() {
+        checkIfTravis();
         String user = db.getDbUser();
-        assertEquals("cs314-db", user);
+        assertEquals(expectedUserName, user);
     }
 
     @Test
-    @DisplayName("DB_Password should be eiK5liet1uej")
+    @DisplayName("DB_Password should be eiK5liet1uej or null if on travis")
     public void testGetDB_Password() {
+        checkIfTravis();
         String password = db.getDbPassword();
-        assertEquals("eiK5liet1uej", password);
+        assertEquals(expectedPassword, password);
     }
 
     @Test
@@ -69,5 +77,21 @@ public class TestQueryDatabase {
     public void testReturnResults() {
         List<Map<String, String>> results = db.returnResults();
         assertEquals(7, results.size());
+    }
+
+    public void checkIfTravis() {
+        if (isTravis != null && isTravis.equals("true")) {
+            expectedURL = "jdbc:mysql://127.0.0.1/cs314";
+            expectedUserName = "root";
+            expectedPassword = null;
+        } else if (useTunnel != null && useTunnel.equals("true")) {
+            expectedURL = "jdbc:mysql://127.0.0.1:56247/cs314";
+            expectedUserName = "cs314-db";
+            expectedPassword = "eiK5liet1uej";
+        } else {
+            expectedURL = "jdbc:mysql://faure.cs.colostate.edu/cs314";
+            expectedUserName = "cs314-db";
+            expectedPassword = "eiK5liet1uej";
+        }
     }
 }
