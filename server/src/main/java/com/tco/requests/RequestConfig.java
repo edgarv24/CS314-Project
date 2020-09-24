0640eb558ac9b64777d98abb211f47a0bd07c13b
Mikayla Powell <70036657+cessna17@users.noreply.github.com>
2020-09-24 13:53:38 -0600

    Update RequestConfig.java

--- server/src/main/java/com/tco/requests/RequestConfig.java
+++ server/src/main/java/com/tco/requests/RequestConfig.java
@@ -3,6 +3,7 @@ package com.tco.requests;
 import com.tco.misc.BadRequestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.ArrayList;
 
 public class RequestConfig extends RequestHeader {
 
