8630d377ef645e683f104e4239bd49acf882769a
Darin Harter <darinh@rams.colostate.edu>
2020-09-28 12:05:04 -0600

    Format RequestFind.java, update buildResponse
    Used auto-format and updated buildResponse with new QueryDatabase method getQueryResults().
    Related: #126

--- server/src/main/java/com/tco/requests/RequestFind.java
+++ server/src/main/java/com/tco/requests/RequestFind.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -19,10 +18,10 @@ public class RequestFind extends RequestHeader {
   private QueryDatabase db;
 
   public RequestFind() {
-      this.requestType = "find";
-      this.requestVersion = RequestHeader.CURRENT_SUPPORTED_VERSION;
-      this.found = null;
-      this.places = null;
+    this.requestType = "find";
+    this.requestVersion = RequestHeader.CURRENT_SUPPORTED_VERSION;
+    this.found = null;
+    this.places = null;
   }
 
   public RequestFind(String match, Integer limit) throws SQLException {
@@ -34,19 +33,29 @@ public class RequestFind extends RequestHeader {
 
   @Override
   public void buildResponse() {
-    log.trace("buildResponse -> {}", this);
     try {
-        db = new QueryDatabase(match);
-        this.places = db.returnResults();
-    }
-    catch (SQLException throwables) {
+      db = new QueryDatabase(match);
+      this.places = db.getQueryResults();
+    } catch (SQLException e) {
       this.places = null;
       this.found = null;
     }
+    log.trace("buildResponse -> {}", this);
   }
 
-  public String getMatch() { return match; }
-  public Integer getLimit() { return limit; }
-  public Integer getFound() { return found; }
-  public List<Map<String, String>> getPlaces() { return places; }
+  public String getMatch() {
+    return match;
+  }
+
+  public Integer getLimit() {
+    return limit;
+  }
+
+  public Integer getFound() {
+    return found;
+  }
+
+  public List<Map<String, String>> getPlaces() {
+    return places;
+  }
 }
