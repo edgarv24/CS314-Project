982192eedacd4fcf93ee7ffaa4d5835c688a3b55
Darin Harter <darinh@rams.colostate.edu>
2020-09-30 20:18:01 -0600

    Cleaned up code, moved buttons, small fixes
    Reformatted file, spaced out buttons more evenly, updated modal appearance, refactor of long methods. Also updated one test in Atlas.test.js.
    Related: #84

--- client/src/components/Atlas/Atlas.js
+++ client/src/components/Atlas/Atlas.js
@@ -1,5 +1,17 @@
 import React, {Component} from 'react';
-import {Button, Col, Container, Modal, ModalBody, ModalHeader, ModalFooter, Row, Input} from 'reactstrap';
+import {
+    Button,
+    Col,
+    Container,
+    Modal,
+    ModalBody,
+    ModalHeader,
+    ModalFooter,
+    Row,
+    Input,
+    InputGroup,
+    InputGroupAddon
+} from 'reactstrap';
 
 import {Map, Marker, Popup, TileLayer, Polyline} from 'react-leaflet';
 
@@ -14,7 +26,7 @@ import LOG from "../../utils/constants"
 import Coordinates from "coordinate-parser";
 
 const MAP_BOUNDS = [[-90, -180], [90, 180]];
-const MAP_CENTER_DEFAULT = [40.5734, -105.0865];
+const MAP_CENTER_DEFAULT = {lat: 40.5734, lng: -105.0865};
 const MARKER_ICON = L.icon({iconUrl: icon, shadowUrl: iconShadow, iconAnchor: [12, 40]});
 const DISTINCT_MARKER = new L.icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
@@ -33,15 +45,13 @@ export default class Atlas extends Component {
         this.setMarker = this.setMarker.bind(this);
         this.setMapToHome = this.setMapToHome.bind(this);
         this.getHomePosition = this.getHomePosition.bind(this);
-        this.getUserMarker = this.getUserMarker.bind(this);
-        this.toggleValue = this.toggleValue.bind(this);
-        this.inputLocations = this.inputLocations.bind(this);
-        this.getInput1 = this.getInput1.bind(this);
-        this.getInput2 = this.getInput2.bind(this);
+
+        this.toggleCoordinateMenu = this.toggleCoordinateMenu.bind(this);
         this.requestDistanceFromServer = this.requestDistanceFromServer.bind(this);
         this.updateInput1 = this.updateInput1.bind(this);
         this.updateInput2 = this.updateInput2.bind(this);
         this.submitDistanceRequest = this.submitDistanceRequest.bind(this);
+
         this.state = {
             userPosition: null,
             markerPosition: null,
@@ -54,20 +64,14 @@ export default class Atlas extends Component {
         };
     }
 
-
-
-    toggleValue(){
-        this.setState({modal: !this.state.modal});
-    }
-
     componentDidMount() {
         // request user location once after first render
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((position) => {
                 this.setState({
+                    userPosition: {lat: position.coords.latitude, lng: position.coords.longitude},
                     markerPosition: {lat: position.coords.latitude, lng: position.coords.longitude},
-                    mapCenter: [position.coords.latitude, position.coords.longitude],
-                    userPosition: [position.coords.latitude, position.coords.longitude]
+                    mapCenter: {lat: position.coords.latitude, lng: position.coords.longitude}
                 })
             });
         }
@@ -82,25 +86,71 @@ export default class Atlas extends Component {
                             {this.renderLeafletMap()}
                         </Col>
                     </Row>
-                    <Row>
-                        <Col className="my-3" sm="12" md={{ size: 6, offset: 5 }}>
-                            <Button color="primary" onClick={this.setMapToHome}>
-                                Where am I?
-                            </Button>{' '}
-                            <Button color="primary" onClick={this.toggleValue}> Find Distance </Button>
-                            <Modal isOpen={this.state.modal} toggle={this.toggleValue} className={this.props.className}>
-                                <ModalHeader toggle={this.toggleValue}> Enter 2 Coordinates </ModalHeader>
-                                <ModalBody>
-                                    {this.inputLocations()}
-                                </ModalBody>
-                                <ModalFooter>
-                                    <Button color='primary' onClick={this.submitDistanceRequest}> Enter </Button>{' '}
-                                    <Button color='primary' onClick={this.toggleValue}> Cancel </Button>
-                                </ModalFooter>
-                            </Modal>
-                        </Col>
-                    </Row>
+                    {this.renderButtons()}
+                    {this.renderCoordinateMenu()}
                 </Container>
+            </div>
+        );
+    }
+
+    renderButtons() {
+        return (
+            <Row className="text-center">
+                <Col className="my-3" xs={{size: 5, offset: 1}}>
+                    <Button color="primary" onClick={this.setMapToHome}>
+                        Where am I?
+                    </Button>{' '}
+                </Col>
+                <Col className="my-3" xs={{size: 5, offset: 0}}>
+                    <Button color="primary" onClick={this.toggleCoordinateMenu}> Find Distance </Button>
+                </Col>
+            </Row>
+        );
+    }
+
+    renderCoordinateMenu() {
+        return (
+            <Row>
+                <Col>
+                    <Modal isOpen={this.state.modal} toggle={this.toggleCoordinateMenu}
+                           className={this.props.className}>
+                        <ModalHeader toggle={this.toggleCoordinateMenu}> Distance Between Coordinates </ModalHeader>
+                        <ModalBody>
+                            {this.renderCoordinateInputs()}
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button className="mr-4" color='primary' onClick={this.toggleCoordinateMenu}> Cancel </Button>
+                            <Button color='primary' onClick={this.submitDistanceRequest}> Enter </Button>
+                        </ModalFooter>
+                    </Modal>
+                </Col>
+            </Row>
+        );
+    }
+
+    renderCoordinateInputs() {
+        return (
+            <div>
+                <Row className="ml-2 mr-2 mt-3 mb-4">
+                    <Col>
+                        <InputGroup>
+                            <InputGroupAddon addonType="prepend">Point 1</InputGroupAddon>
+                            <Input placeholder="0.0, 0.0"
+                                   onChange={(e) => () => this.setState({input1: e.target.value})}
+                                   value={this.state.input1}/>
+                        </InputGroup>
+                    </Col>
+                </Row>
+                <Row className="ml-2 mr-2 mb-3">
+                    <Col>
+                        <InputGroup>
+                            <InputGroupAddon addonType="prepend">Point 2</InputGroupAddon>
+                            <Input placeholder="0.0, 0.0"
+                                   onChange={(e) => () => this.setState({input2: e.target.value})}
+                                   value={this.state.input2}/>
+                        </InputGroup>
+                    </Col>
+                </Row>
             </div>
         );
     }
@@ -121,29 +171,30 @@ export default class Atlas extends Component {
                 onClick={this.setMarker}
             >
                 <TileLayer url={MAP_LAYER_URL} attribution={MAP_LAYER_ATTRIBUTION}/>
-                {this.getFirstMarker()}
-                {this.getSecondMarker()}
-                {this.getUserMarker()}
+                {this.getMarker(this.state.markerPosition, MARKER_ICON, true)}
+                {this.getMarker(this.state.secondMarkerPosition, MARKER_ICON, true)}
+                {this.getMarker(this.state.userPosition, DISTINCT_MARKER, false)}
                 {this.renderPolyline()}
-                //add box with distance here
             </Map>
         );
     }
 
+    toggleCoordinateMenu() {
+        this.setState({modal: !this.state.modal});
+    }
+
     setMarker(mapClickInfo) {
-        if(!this.state.markerPosition) {
+        if (!this.state.markerPosition) {
             this.setState({
                 markerPosition: mapClickInfo.latlng,
                 mapCenter: mapClickInfo.latlng
             });
-        }
-        else if (!this.state.secondMarkerPosition) {
-            this.setState( {
+        } else if (!this.state.secondMarkerPosition) {
+            this.setState({
                 secondMarkerPosition: mapClickInfo.latlng,
                 mapCenter: mapClickInfo.latlng
             });
-        }
-        else {
+        } else {
             this.setState({
                 markerPosition: this.state.secondMarkerPosition,
                 secondMarkerPosition: mapClickInfo.latlng,
@@ -152,47 +203,32 @@ export default class Atlas extends Component {
         }
     }
 
-    getFirstMarker() {
+    getMarker(position, iconStyle, usePopup) {
         const initMarker = ref => {
-            if (ref) {
+            if (ref && usePopup) {
                 ref.leafletElement.openPopup()
             }
         };
 
-        if (this.state.markerPosition) {
+        if (position) {
             return (
-                <Marker ref={initMarker} position={this.state.markerPosition} icon={MARKER_ICON}>
-                    <Popup offset={[0, -18]} className="font-weight-bold">{this.getStringMarkerPosition()}</Popup>
+                <Marker ref={initMarker} position={position} icon={iconStyle}>
+                    <Popup offset={[0, -18]}
+                           className="font-weight-bold">{this.getStringMarkerPosition(position)}</Popup>
                 </Marker>
             );
         }
     }
 
-    getSecondMarker() {
-        const initMarker = ref => {
-            if (ref) {
-                ref.leafletElement.openPopup()
-            }
-        };
-
-        if (this.state.secondMarkerPosition) {
-            return (
-                <Marker ref={initMarker} position={this.state.secondMarkerPosition} icon={MARKER_ICON}>
-                    <Popup offset={[0, -18]} className="font-weight-bold">{this.getStringMarkerPosition()}</Popup>
-                </Marker>
-            );
-        }
-    }
-
-    getStringMarkerPosition() {
-        return this.state.markerPosition.lat.toFixed(2) + ', ' + this.state.markerPosition.lng.toFixed(2);
+    getStringMarkerPosition(position) {
+        return position.lat.toFixed(2) + ', ' + position.lng.toFixed(2);
     }
 
     setMapToHome() {
         let homePos = this.getHomePosition();
         this.setState({
-            secondMarkerPosition: {lat: homePos[0], lng: homePos[1]},
-            mapCenter: homePos
+            secondMarkerPosition: {lat: homePos["lat"], lng: homePos["lng"]},
+            mapCenter: {lat: homePos["lat"], lng: homePos["lng"]}
         });
     }
 
@@ -202,57 +238,18 @@ export default class Atlas extends Component {
         return MAP_CENTER_DEFAULT;
     }
 
-    getUserMarker() {
-        if (this.state.userPosition) {
-            return (
-                <Marker position={this.state.userPosition} icon={DISTINCT_MARKER}>
-                    <Popup offset={[0, -18]} className="font-weight-bold">{this.getStringMarkerPosition()}</Popup>
-                </Marker>
-            );
-        }
-    }
     renderPolyline() {
         if (this.state.markerPosition && this.state.secondMarkerPosition) {
             return (
                 <Polyline color={'red'} positions={
                     [[this.state.markerPosition.lat, this.state.markerPosition.lng],
-                    [this.state.secondMarkerPosition.lat, this.state.secondMarkerPosition.lng]]}>
+                        [this.state.secondMarkerPosition.lat, this.state.secondMarkerPosition.lng]]}>
                 </Polyline>
             );
         }
     }
 
-    inputLocations(){
-        return (
-            <Row className="m-2">
-                <Col xs={12}>
-                    {this.getInput1()}
-                </Col>
-                <Col xs={12}>
-                    {this.getInput2()}
-                </Col>
-            </Row>
-        );
-    }
-
-    getInput1(){
-        return(
-            <Input placeholder="Enter first coordinates" onChange={(e) => (() => this.setState({input1: e.target.value}))}
-                   value={this.state.input1}
-            />
-        );
-    }
-
-    getInput2(){
-        return(
-            <Input placeholder="Enter second coordinates" onChange={(e) => (() => this.setState({input2: e.target.value}))}
-                   value={this.state.input2}
-            />
-        );
-    }
-
     isValidPosition(position) {
-        let error;
         let isValid;
         try {
             isValid = true;
@@ -265,54 +262,64 @@ export default class Atlas extends Component {
     };
 
     updateInput1() {
-        if(this.isValidPosition(this.state.input1)){
+        if (this.isValidPosition(this.state.input1)) {
             const position1 = new Coordinates(this.state.input1);
             const coord1 = {lat: position1.getLatitude(), lng: position1.getLongitude()};
 
             this.setState({markerPosition: coord1});
-        } else{
+        } else {
             this.setState({markerPosition: {lat: 10, lng: -35}});
         }
 
     }
 
     updateInput2() {
-        if(this.isValidPosition(this.state.input2)){
+        if (this.isValidPosition(this.state.input2)) {
             const position2 = new Coordinates(this.state.input2);
             const coord2 = {lat: position2.getLatitude(), lng: position2.getLongitude()};
 
             this.setState({secondMarkerPosition: coord2});
-        }else{
+        } else {
             this.setState({secondMarkerPosition: {lat: -10, lng: 36}});
         }
 
     }
 
-    submitDistanceRequest(){
+    submitDistanceRequest() {
         this.updateInput1();
         this.updateInput2();
 
         this.requestDistanceFromServer();
     }
 
-    requestDistanceFromServer(){
+    requestDistanceFromServer() {
         let place1Pos = {latitude: this.state.markerPosition["lat"], longitude: this.state.markerPosition["lng"]};
-        let place2Pos = {latitude: this.state.secondMarkerPosition["lat"], longitude: this.state.secondMarkerPosition["lng"]};
+        let place2Pos = {
+            latitude: this.state.secondMarkerPosition["lat"],
+            longitude: this.state.secondMarkerPosition["lng"]
+        };
         let distResult = null;
-        sendServerRequest({requestType: "distance", requestVersion: 2, place1: place1Pos, place2: place2Pos, earthRadius: 3959})
+        sendServerRequest({
+            requestType: "distance",
+            requestVersion: 2,
+            place1: place1Pos,
+            place2: place2Pos,
+            earthRadius: 3959.0
+        })
             .then(distance => {
-                if (distance) { if(this.validDistanceResponse(distance.data)){ distResult = distance;} }
-                else { this.setState({distanceLabel: null}); }
+                if (distance) {
+                    if (this.validDistanceResponse(distance.data)) {
+                        distResult = distance;
+                    }
+                } else {
+                    this.setState({distanceLabel: null});
+                }
             });
         this.setState({distanceLabel: distResult});
         LOG.info(distResult);
     }
 
     validDistanceResponse(distance) {
-        if(!isJsonResponseValid(distance, distanceSchema)) {
-            return false;
-        } else {
-            return true;
-        }
+        return isJsonResponseValid(distance, distanceSchema);
     }
 }
