4c214b9f4320976f661264db604a8a0791db8ac3
Edgar Varela <edgar.varela@rams.colostate.edu>
2020-10-08 19:36:08 -0600

    Created RequestTrip.java to handle Trip Requests. Added tests to TestRequestTrip.java. Closes #159

---
+++ server/src/main/java/com/tco/requests/RequestTrip.java
@@ -0,0 +1,68 @@
+package com.tco.requests;
+
+import com.tco.misc.BadRequestException;
+import com.tco.misc.CalculateDistance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class RequestTrip extends RequestHeader {
+  private Map<String, String> options;
+  private List<Map<String, String>> places;
+  private List<Integer> distances;
+  private CalculateDistance cd;
+
+  private final transient Logger log = LoggerFactory.getLogger(RequestTrip.class);
+
+  public RequestTrip() {
+    this.requestType = "trip";
+    this.requestVersion = RequestHeader.CURRENT_SUPPORTED_VERSION;
+  }
+
+  public RequestTrip(Map<String, String> options, List<Map<String, String>> places) {
+    this();
+    this.options = options;
+    this.places = places;
+  }
+
+  @Override
+  public void buildResponse() throws BadRequestException {
+    buildDistanceList(options, places);
+    log.trace("buildResponse -> {}", this);
+  }
+
+  public void buildDistanceList(Map<String, String> options, List<Map<String, String>> places) {
+    distances = new ArrayList<>();
+    Double radius = Double.parseDouble(options.get("earthRadius"));
+    cd = CalculateDistance.usingRadius(radius);
+    for (int i = 0; i < places.size(); i++) {
+      double lat1 = Double.parseDouble(places.get(i).get("latitude"));
+      double long1 = Double.parseDouble(places.get(i).get("longitude"));
+      double lat2;
+      double long2;
+      if (i != places.size() - 1) {
+        lat2 = Double.parseDouble(places.get(i + 1).get("latitude"));
+        long2 = Double.parseDouble(places.get(i + 1).get("longitude"));
+      } else {
+        lat2 = Double.parseDouble(places.get(0).get("latitude"));
+        long2 = Double.parseDouble(places.get(0).get("longitude"));
+      }
+      distances.add((int) cd.distBetween(lat1, long1, lat2, long2));
+    }
+  }
+
+  public Map<String, String> getOptions() {
+    return this.options;
+  }
+
+  public List<Map<String, String>> getPlaces() {
+    return this.places;
+  }
+
+  public List<Integer> getDistances() {
+    return this.distances;
+  }
+}
---
+++ server/src/test/java/com/tco/requests/TestRequestTrip.java
@@ -0,0 +1,110 @@
+package com.tco.requests;
+
+import com.tco.misc.BadRequestException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestRequestTrip {
+  private RequestTrip requestTrip;
+  Map<String, String> options;
+  List<Map<String, String>> places;
+
+  @BeforeEach
+  public void createConfigurationForTests() {
+    options = new HashMap<>();
+    options.put("title", "My Trip");
+    options.put("earthRadius", "3959.0");
+
+    places = new ArrayList<>();
+    Map<String, String> placesEntry1 =
+        Map.of(
+            "name", "Denver",
+            "latitude", "39.7",
+            "longitude", "-105.0");
+    Map<String, String> placesEntry2 =
+        Map.of(
+            "name", "Boulder",
+            "latitude", "40.0",
+            "longitude", "-105.4");
+    Map<String, String> placesEntry3 =
+        Map.of(
+            "name", "Fort Collins",
+            "latitude", "40.6",
+            "longitude", "-105.1");
+    places.add(placesEntry1);
+    places.add(placesEntry2);
+    places.add(placesEntry3);
+
+    requestTrip = new RequestTrip(options, places);
+  }
+
+  @Test
+  @DisplayName("Testing constructor")
+  public void testConstructor() {
+    assertEquals(options, requestTrip.getOptions());
+    assertEquals(places, requestTrip.getPlaces());
+  }
+
+  @Test
+  @DisplayName("Testing distance list with given CO trip")
+  public void testCorrectDistanceList() throws BadRequestException {
+    List<Integer> results = Arrays.asList(29, 44, 62);
+    requestTrip.buildResponse();
+    assertEquals(3, requestTrip.getDistances().size());
+    assertEquals(results, requestTrip.getDistances());
+  }
+
+  @Test
+  @DisplayName("Testing for two identical places return [0,0]")
+  public void testSameLocations() throws BadRequestException {
+    places.remove(2);
+    places.remove(1);
+    Map<String, String> denver = Map.of(
+            "name", "Denver",
+            "latitude", "39.7",
+            "longitude", "-105.0"
+    );
+    places.add(denver);
+    requestTrip = new RequestTrip(options, places);
+    requestTrip.buildResponse();
+    List<Integer> results = Arrays.asList(0, 0);
+    assertEquals(2, requestTrip.getDistances().size());
+    assertEquals(results, requestTrip.getDistances());
+  }
+
+  @Test
+  @DisplayName("Testing with earthRadius in feet and delta being 100ft")
+  public void testBigEarthRadius() throws BadRequestException {
+    options.clear();
+    options.put("title", "My Trip");
+    options.put("earthRadius", "20902230");
+    requestTrip = new RequestTrip(options, places);
+    requestTrip.buildResponse();
+    List<Integer> results = Arrays.asList(156604, 234273, 329472);
+
+    assertEquals(results.get(0), requestTrip.getDistances().get(0), 100.0);
+    assertEquals(results.get(1), requestTrip.getDistances().get(1), 100.0);
+    assertEquals(results.get(2), requestTrip.getDistances().get(2), 100.0);
+  }
+
+  @Test
+  @DisplayName("Testing with earthRadius in millimeters and delta being 100ft")
+  public void testSmallEarthRadius() throws BadRequestException {
+    options.clear();
+    options.put("title", "My Trip");
+    options.put("earthRadius", "6371008771");
+    requestTrip = new RequestTrip(options, places);
+    requestTrip.buildResponse();
+    List<Integer> results = Arrays.asList(47733143, 71406593, 100420000);
+
+    assertEquals(results.get(0), requestTrip.getDistances().get(0), 30480);
+    assertEquals(results.get(1), requestTrip.getDistances().get(1), 30480);
+    assertEquals(results.get(2), requestTrip.getDistances().get(2), 30480);
+  }
+}
