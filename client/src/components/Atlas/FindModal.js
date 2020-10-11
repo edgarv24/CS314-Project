1b34d1169e3e376193621d631c00624b48a237ac
Edgar Varela <edgar.varela@rams.colostate.edu>
2020-10-11 17:04:05 -0600

    Added Find Component. Closes #92

---
+++ client/src/components/Atlas/FindModal.js
@@ -0,0 +1,112 @@
+import React, {Component} from 'react';
+import {
+    Button,
+    Col,
+    Input,
+    InputGroup,
+    InputGroupAddon,
+    Modal,
+    ModalBody,
+    ModalFooter,
+    ModalHeader,
+    Row,
+} from 'reactstrap';
+
+import {isJsonResponseValid, sendServerRequest} from "../../utils/restfulAPI";
+import * as findSchema from "../../../schemas/FindResponse.json";
+
+import {PROTOCOL_VERSION} from "../../utils/constants";
+
+export default class DistanceModal extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            places: null,
+            inputText: null
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <Modal isOpen={this.props.isOpen} toggle={() => this.props.toggleOpen()}>
+                    <ModalHeader className="mt-1" toggle={() => this.props.toggleOpen()}>
+                        <span className="ml-4">Find Places</span>
+                    </ModalHeader>
+                    <ModalBody>
+                        <div>
+                            {this.renderInputBox()}
+                        </div>
+                    </ModalBody>
+                    {this.renderSearchButton()}
+                </Modal>
+            </div>
+        );
+    }
+
+    renderInputBox() {
+        return (
+            <Row className="ml-2 mr-2 mt-3 mb-4">
+                <Col>
+                    <InputGroup>
+                        <InputGroupAddon addonType="prepend">{`Name`}</InputGroupAddon>
+                        <Input placeholder="Enter place"
+                               onChange={ e => this.setState({inputText: e.target.value}) }
+                               value={this.state.inputText || ""}
+                        />
+                    </InputGroup>
+                </Col>
+            </Row>
+        );
+    }
+
+    renderSearchButton() {
+        return (
+            <ModalFooter>
+                <Button className="mr-2" color='primary' onClick={() => this.resetModalState()}>Cancel</Button>
+                <Button color='primary' onClick={this.requestFindFromServer(this.state.inputText)}
+                >
+                    Submit
+                </Button>
+            </ModalFooter>
+        );
+    }
+
+    resetModalState() {
+        this.props.toggleOpen();
+        this.setState({
+            places: null,
+            inputText: null,
+        });
+    }
+
+    requestFindFromServer(placeName) {
+        sendServerRequest(this.constructRequestBody(placeName))
+            .then(responseJSON => {
+                if (responseJSON) {
+                    this.processFindResponse(responseJSON)
+                }
+            }
+        );
+    }
+
+    constructRequestBody(placeName) {
+        return {
+            requestVersion: PROTOCOL_VERSION,
+            requestType: "find",
+            match: placeName,
+            limit: 10,
+        }
+    }
+
+    processFindResponse(responseJSON) {
+        const responseBody = responseJSON.data;
+        if (isJsonResponseValid(responseBody, findSchema)) {
+            this.setState({places: responseBody.places});
+        } else {
+            this.setState({places: null});
+        }
+    }
+}
