dc3bec445585b37611baf168ce03fee46992bc1d
Edgar Varela <edgar.varela@rams.colostate.edu>
2020-10-16 15:38:44 -0600

    Added Input test to FindModal.test.js. Related #88

--- client/test/FindModal.test.js
+++ client/test/FindModal.test.js
@@ -2,10 +2,10 @@ import './jestConfig/enzyme.config.js';
 
 import React from 'react';
 import {shallow} from 'enzyme';
-import axios from 'axios';
+
 
 import FindModal from '../src/components/Atlas/FindModal';
-import {jest} from "@jest/globals";
+
 
 function testInitialFindModalState() {
     const findModal = shallow(<FindModal/>);
@@ -45,4 +45,20 @@ function testInputBox() {
 
 test("Testing input box", testInputBox);
 
+function testListSize() {
+    const findModal = shallow(<FindModal/>);
+    let firstResponse = '{"data": {"requestType": "find", "requestVersion": 3, '+
+        '                "found": 3, "places": [{"name": "Place1", "latitude": "90", "longitude": "100"}]}}';
+    findModal.instance().processFindResponse(JSON.parse(firstResponse));
+    findModal.update();
+    expect(findModal.state().places.length).toEqual(1);
+    let secondResponse = '{"data": {"requestType": "find", "requestVersion": 3, ' +
+        '                  "found": 3, "places": [{"name": "Place1", "latitude": "90", '+
+        '                  "longitude": "100"},{"name": "Place2", "latitude": "90", '+
+        '                  "longitude": "100"},{"name": "Place3", "latitude": "90", "longitude": "100"}]}}';
+    findModal.instance().processFindResponse(JSON.parse(secondResponse));
+    findModal.update();
+    expect(findModal.state().places.length).toEqual(3);
+}
 
+test("Testing input list size after find request", testListSize);
