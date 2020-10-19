35260999a0fa30fecd9460e418440d83cdd0548e
Darin Harter <darinh@rams.colostate.edu>
2020-10-19 17:08:23 -0600

    Add new methods and tests to Trip.js
    addPlaces, removeAtIndex, coordinatesList, itineraryData. Additional tests for each.
    
    Related: #163

--- client/src/components/Atlas/Trip.js
+++ client/src/components/Atlas/Trip.js
@@ -1,4 +1,5 @@
 import {PROTOCOL_VERSION} from "../../utils/constants";
+import Coordinates from "coordinate-parser";
 
 export default class Trip {
     constructor() {
@@ -9,36 +10,48 @@ export default class Trip {
         this.distances = [];
     }
 
-    get title() {
-        return this.options.title;
+    addPlace(place) {
+        return this.addPlaces([place]);
     }
 
-    get earthRadius() {
-        return this.options.earthRadius;
-    }
-
-    addDestination(destination) {
-        const newTrip = this.copy(this);
-        newTrip.places = [...this.places, destination];
+    addPlaces(places) {
+        const newTrip = this.copy();
+        newTrip.places = this.places.concat(places);
+        // recalculate distances with server request
         return newTrip;
     }
 
-    copy(prevTrip) {
+    removeAtIndex(index) {
+        if (index < 0 || index >= this.places.length)
+            return this;
+
+        const newTrip = this.copy();
+        newTrip.places.splice(index, 1);
+        return newTrip;
+    }
+
+    copy() {
         let newTrip = new Trip();
-        newTrip.requestVersion = prevTrip.requestVersion;
-        newTrip.requestType = prevTrip.requestType;
-        newTrip.options = {title: prevTrip.title, earthRadius: prevTrip.options.earthRadius};
+        newTrip.requestVersion = this.requestVersion;
+        newTrip.requestType = this.requestType;
+        newTrip.options = {title: this.title, earthRadius: this.options.earthRadius};
         newTrip.places = [];
-        prevTrip.places.forEach(item => {
+        this.places.forEach(item => {
             newTrip.places.push(JSON.parse(JSON.stringify(item)));
         });
-        newTrip.distances = prevTrip.distances;
+        newTrip.distances = this.distances;
         return newTrip;
     }
 
      setTitle(newTitle) {
-        let newTrip = this.copy(this);
+        let newTrip = this.copy();
         newTrip.options.title = newTitle;
+        return newTrip;
+    }
+
+    setDistances(distances) {
+        let newTrip = this.copy();
+        newTrip.distances = distances;
         return newTrip;
     }
 
@@ -46,20 +59,14 @@ export default class Trip {
         if (this.places.length === 0 || index > this.places.length)
             return this;
         else {
-            let newTrip = this.copy(this);
+            let newTrip = this.copy();
             newTrip.places[index].notes = note;
             return newTrip;
         }
     }
 
-    setDistance(distances) {
-        let newTrip = this.copy(this);
-        newTrip.distances = distances;
-        return newTrip;
-    }
-
     loadJSON(tripFile) {
-        let newTrip = this.copy(this);
+        let newTrip = this.copy();
         newTrip.requestVersion = tripFile.requestVersion;
         newTrip.requestType = tripFile.requestVersion;
         newTrip.options = tripFile.options;
@@ -70,6 +77,52 @@ export default class Trip {
 
     build() {
         return JSON.parse(JSON.stringify(this));
+    }
+
+    get title() {
+        return this.options.title;
+    }
+
+    get earthRadius() {
+        return this.options.earthRadius;
+    }
+
+    get coordinatesList() {
+        return this.places.map((place) => {
+            const coordinate = new Coordinates(`${place.latitude}, ${place.longitude}`);
+            return {lat: coordinate.getLatitude(), lng: coordinate.getLongitude()};
+        });
+    }
+
+    get itineraryPlaceData() {
+        let distancesAreCalculated = this.distances.length > 0;
+        let cumulativeDist = 0;
+
+        let placesData = [];
+        for (let i = 0; i <= this.places.length; i++) {
+            const place = this.places[i % this.places.length];
+            const legDist = (distancesAreCalculated && index > 0) ? this.distances[index - 1] : 0;
+            cumulativeDist += legDist;
+
+            placesData.push(this.fullJSON(place, legDist, cumulativeDist, i));
+        }
+        return placesData;
+    }
+
+    fullJSON(place, legDist, cumulativeDist, index) {
+        return {
+            "id": `destination-${index + 1}`,
+            "name": place.name,
+            "latitude": place.latitude,
+            "longitude": place.longitude,
+            "municipality": place.municipality || "",
+            "state": place.state || "",
+            "country": place.country || "",
+            "altitude": place.altitude || "",
+            "notes": place.notes || "",
+            "leg_dist": legDist,
+            "cumulative_dist": cumulativeDist
+        };
     }
 }
 
--- client/test/Trip.test.js
+++ client/test/Trip.test.js
@@ -14,8 +14,8 @@ describe('Trip', () => {
     });
 
     it('initializes request info with no-args constructor', () => {
-       expect(trip.requestVersion).toEqual(3);
-       expect(trip.requestType).toEqual('trip');
+        expect(trip.requestVersion).toEqual(3);
+        expect(trip.requestType).toEqual('trip');
     });
 
     it('sets title of trip to "My Trip" with no-args constructor', () => {
@@ -23,7 +23,7 @@ describe('Trip', () => {
     });
 
     it('sets earthRadius to 3959.0 by default', () => {
-       expect(trip.options.earthRadius).toEqual('3959.0');
+        expect(trip.options.earthRadius).toEqual('3959.0');
     });
 
     it('sets places to empty array with no-args constructor', () => {
@@ -36,26 +36,47 @@ describe('Trip', () => {
         expect(trip.distances).toEqual(emptyArray);
     });
 
-    it('gets earthradius', () => {
-       let expectedEarthRadius = '3959.0';
-       let actualEarthRadius = trip.earthRadius;
-       expect(actualEarthRadius).toEqual(expectedEarthRadius);
+    it('gets earth radius', () => {
+        let expectedEarthRadius = '3959.0';
+        let actualEarthRadius = trip.earthRadius;
+        expect(actualEarthRadius).toEqual(expectedEarthRadius);
     });
 
     it('adds new places to a new trip object', () => {
-        let destination = {
+        let place = {
             'name': 'Fort Collins',
             'latitude': '40.5853',
             'longitude': '-105.0844'
         };
-        let newTrip = trip.addDestination(destination);
+        let newTrip = trip.addPlace(place);
         expect(trip.places.length).toEqual(0);
         expect(newTrip.places.length).toEqual(1);
     });
 
-    it('copies objects', () => {
-        const newTrip = trip.copy(trip);
-        expect(newTrip).toEqual(trip);
+    it('adds multiple places at once', () => {
+        let newTrip = trip.addPlace({'name': 'a', 'latitude': '0', 'longitude': 0});
+        expect(trip.places.length).toEqual(0);
+        expect(newTrip.places.length).toEqual(1);
+
+        const places = [{'name': 'Place 1', 'latitude': '0', 'longitude': '0'},
+            {'name': 'Place 2', 'latitude': '0', 'longitude': '0'},
+            {'name': 'Place 3', 'latitude': '0', 'longitude': '0'}];
+        newTrip = newTrip.addPlaces(places);
+        expect(trip.places.length).toEqual(0);
+        expect(newTrip.places.length).toEqual(4);
+    });
+
+    it('removes a places at an index', () => {
+        const p1 = {'name': 'Place 1', 'latitude': '0', 'longitude': '0'};
+        const p2 = {'name': 'Place 2', 'latitude': '0', 'longitude': '0'};
+        const p3 = {'name': 'Place 3', 'latitude': '0', 'longitude': '0'};
+        let newTrip = trip.addPlaces([p1, p2, p3]);
+        expect(trip.places.length).toEqual(0);
+        expect(newTrip.places.length).toEqual(3);
+
+        newTrip = newTrip.removeAtIndex(1);
+        expect(newTrip.places.length).toEqual(2);
+        expect(newTrip.places[1].name).toEqual('Place 3');
     });
 
     it('sets title of a trip', () => {
@@ -66,6 +87,11 @@ describe('Trip', () => {
         expect(newTrip.title).toEqual(newTripTitle);
     });
 
+    it('copies objects', () => {
+        const newTrip = trip.copy();
+        expect(newTrip).toEqual(trip);
+    });
+
     it('returns original object if notes assigned to empty places array', () => {
         let note = 'Home of CSU';
         const newTrip = trip.addNote(0, note);
@@ -73,46 +99,63 @@ describe('Trip', () => {
     });
 
     it('adds note to place', () => {
-        let destination = {
+        let place = {
             'name': 'Fort Collins',
             'latitude': '40.5853',
             'longitude': '-105.0844'
         };
         let note = 'Home of CSU';
-        let newTrip = trip.addDestination(destination).addNote(0, note);
+        let newTrip = trip.addPlace(place).addNote(0, note);
         expect(newTrip.places[0].notes).toEqual(note);
     });
 
     it('returns original object if index greater than length', () => {
-        let destination = {
+        let place = {
             'name': 'Fort Collins',
             'latitude': '40.5853',
             'longitude': '-105.0844'
         };
         let note = 'Home of CSU';
-        let temp = trip.addDestination(destination);
+        let temp = trip.addPlace(place);
         let newTrip = temp.addNote(10, note);
         expect(newTrip).toEqual(temp);
     })
 
     it('sets distances', () => {
         let expectedDistances = [1, 2, 3];
-        let newTrip = trip.setDistance(expectedDistances);
+        let newTrip = trip.setDistances(expectedDistances);
         let actualDistances = newTrip.distances;
         expect(actualDistances).toEqual(expectedDistances);
     });
 
     it('loads a JSON file into properties', () => {
         let tripFile = {
-            "requestType"    : "trip",
-            "requestVersion" : 3,
-            "options"        : { "title":"Shopping Loop",
-            "earthRadius":"3959.0"
-        },
-            "places"         : [{"name":"Denver",       "latitude": "39° 44' 31.3548'' N", "longitude": "104° 59' 29.5116'' W", "notes":"The big city"},
-            {"name":"Boulder",      "latitude": "40° 0' 53.9424'' N", "longitude": "105° 16' 13.9656'' W", "notes":"Home of CU"},
-            {"name":"Fort Collins", "latitude": "40° 35' 6.9288'' N", "longitude": "105° 5' 3.9084'' W", "notes":"Home of CSU"}],
-            "distances"      : [20, 40, 50]
+            "requestType": "trip",
+            "requestVersion": 3,
+            "options": {
+                "title": "Shopping Loop",
+                "earthRadius": "3959.0"
+            },
+            "places": [
+                {
+                    "name": "Denver",
+                    "latitude": "39° 44' 31.3548'' N",
+                    "longitude": "104° 59' 29.5116'' W",
+                    "notes": "The big city"
+                },
+                {
+                    "name": "Boulder",
+                    "latitude": "40° 0' 53.9424'' N",
+                    "longitude": "105° 16' 13.9656'' W",
+                    "notes": "Home of CU"
+                },
+                {
+                    "name": "Fort Collins",
+                    "latitude": "40° 35' 6.9288'' N",
+                    "longitude": "105° 5' 3.9084'' W",
+                    "notes": "Home of CSU"
+                }],
+            "distances": [20, 40, 50]
         }
         let newTrip = trip.loadJSON(tripFile);
         expect(newTrip.title).toEqual('Shopping Loop');
@@ -121,16 +164,45 @@ describe('Trip', () => {
     });
 
     it('creates a JSON tripFile', () => {
-       let expectedJSON = {
-           "requestType"    : "trip",
-           "requestVersion" : 3,
-           "options"        : { "title":"My Trip",
-               "earthRadius":"3959.0"
-           },
-           "places"         : [],
-           "distances"      : []
-       }
-       let actualJSON = trip.build();
-       expect(actualJSON).toEqual(expectedJSON);
+        let expectedJSON = {
+            "requestType": "trip",
+            "requestVersion": 3,
+            "options": {
+                "title": "My Trip",
+                "earthRadius": "3959.0"
+            },
+            "places": [],
+            "distances": []
+        }
+        let actualJSON = trip.build();
+        expect(actualJSON).toEqual(expectedJSON);
+    });
+
+    it('returns a list of coordinate objects', () => {
+        const p1 = {'name': 'Place 1', 'latitude': '10', 'longitude': '40'};
+        const p2 = {'name': 'Place 2', 'latitude': '20', 'longitude': '50'};
+        const p3 = {'name': 'Place 3', 'latitude': '30', 'longitude': '60'};
+
+        const newTrip = trip.addPlaces([p1, p2, p3]);
+        const expectedList = [{lat: 10, lng: 40}, {lat: 20, lng: 50}, {lat: 30, lng: 60}];
+        expect(newTrip.coordinatesList).toEqual(expectedList);
+    });
+
+    it('returns place data compatible with the itinerary', () => {
+        const p1 = {'name': 'Water Park', 'municipality': 'London', 'country': 'England',
+            'latitude': '8', 'longitude': '24', 'notes': 'fun'};
+        const p2 = {'name': 'Zoo', 'municipality': 'Denver', 'state': 'Colorado', 'country': 'United States',
+            'latitude': '3', 'longitude': '7'};
+
+        const newTrip = trip.addPlaces([p1, p2]);
+        const itineraryData = newTrip.itineraryPlaceData;
+
+        const expected1 = {"id": "destination-1", "name": "Water Park", "latitude": "8", "longitude": "24", "municipality": "London",
+            "state": "", "country": "England", "altitude": "", "notes": "fun", "leg_dist": 0, "cumulative_dist": 0};
+        const expected2 = {"id": "destination-2", "name": "Zoo", "latitude": "3", "longitude": "7", "municipality": "Denver",
+            "state": "Colorado", "country": "United States", "altitude": "", "notes": "", "leg_dist": 0, "cumulative_dist": 0};
+
+        expect(itineraryData[0]).toEqual(expected1);
+        expect(itineraryData[1]).toEqual(expected2);
     });
 });
