798592531e0ecb03d60c0e08908d3097ffd41c60
rylie <rylie.denehan@gmail.com>
2020-10-30 15:07:20 -0600

    Initialized filters.

--- server/src/main/java/com/tco/requests/RequestConfig.java
+++ server/src/main/java/com/tco/requests/RequestConfig.java
@@ -3,16 +3,14 @@ package com.tco.requests;
 import com.tco.misc.BadRequestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+
+import java.util.*;
 
 public class RequestConfig extends RequestHeader {
 
   private String serverName;
   private final ArrayList<String> supportedRequests = new ArrayList<>();
-  private Map<String,List<String>> filters;
+  private Map<String,List<String>> filters = new HashMap<>();
   private final transient Logger log = LoggerFactory.getLogger(RequestConfig.class);
 
   public RequestConfig() {
