57484d320a73e934bbb34b42b09981a2467d9b2f
Darin Harter <70183486+darin3@users.noreply.github.com>
2020-11-05 22:27:05 -0700

    Remove unused import

--- server/src/main/java/com/tco/requests/RequestConfig.java
+++ server/src/main/java/com/tco/requests/RequestConfig.java
@@ -1,6 +1,5 @@
 package com.tco.requests;
 
-import com.sun.org.apache.bcel.internal.generic.Select;
 import com.tco.misc.BadRequestException;
 import com.tco.misc.QueryDatabase;
 import org.slf4j.Logger;
@@ -31,10 +30,10 @@ public class RequestConfig extends RequestHeader {
       this.supportedRequests.add("trip");
       log.trace("buildResponse -> {}", this);
       filters.put("type", Arrays.asList("airport", "balloonport", "heliport"));
-      try{
+      try {
         filters.put("where", QueryDatabase.getCountryList());
       }
-      catch (SQLException e){
+      catch (SQLException e) {
         throw new BadRequestException();
       }
   }
