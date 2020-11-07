c599d66dba18e6e50da103f021dbc5e641e34bb0
Darin Harter <darinh@rams.colostate.edu>
2020-11-07 08:50:36 -0700

    Add units and helpers to constants.js Related #337

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -14,7 +14,22 @@ export const LOG = ulog("App");
 
 export const PROTOCOL_VERSION = 4;
 export const CLIENT_TEAM_NAME = "T14 The Fourteeners";
-export const EARTH_RADIUS_UNITS_DEFAULT = {"miles": 3959.0};
+
+export const EARTH_RADIUS_UNITS_DEFAULT = {"nautical miles": 3440.0, "miles": 3959.0, "kilometers": 6371.0,
+    "yards": 6967420.0, "feet": 20902260.0, "inches": 250827116.0};
+
+export const UNITS_SINGULAR = {"nautical miles": "nautical mile", "miles": "mile", "kilometers": "kilometer",
+    "yards": "yard", "feet": "foot", "inches": "inch"};
+
+export const correctUnits = (unit, value) => {
+    if (value === 1 && Object.keys(UNITS_SINGULAR).includes(unit))
+        return UNITS_SINGULAR[unit];
+    return unit;
+}
+
+export const capitalize = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
 
 export const HTTP_OK = 200;
 export const HTTP_BAD_REQUEST = 400;
