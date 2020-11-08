bec1dbc5ffc4a39f210a294c06b7e9bd08ba6cc8
Darin Harter <darinh@rams.colostate.edu>
2020-11-08 11:03:02 -0700

    Add save, load, and unit functionality to TripSettingsModal
    Change unit selector to choose from default units list. Add save button to download trip JSON. Add file uploader for loading a trip. Add world peaks pre-loaded trip button. Add button to clear trip data.
    
    Related #276, #277, #278

--- client/src/components/Atlas/Modals/TripSettingsModal.js
+++ client/src/components/Atlas/Modals/TripSettingsModal.js
@@ -1,19 +1,29 @@
 import React from "react";
-import {Button, Input, InputGroup, InputGroupAddon, Modal, ModalBody, ModalFooter, Row, Col} from "reactstrap";
+import {Button, Input, InputGroup, Modal, ModalBody, ModalFooter, Row, Col} from "reactstrap";
+import {FormFeedback, Label} from "reactstrap";
 
 import {renderModalTitleHeader, renderCancelButton} from "./modalHelper";
 
 import coBrews from "../../../../test/TripFiles/co-brews.json";
 import usBrews from "../../../../test/TripFiles/us-brews.json";
 import worldBrews from "../../../../test/TripFiles/world-brews.json";
+import worldPeaks from "../../../../test/TripFiles/world-peaks.json";
+import Trip from "../Trip";
+import {LOG, EARTH_RADIUS_UNITS_DEFAULT, capitalize} from "../../../utils/constants";
 
 export default class TripSettingsModal extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            titleInput: ""
+            titleInput: "",
+            invalidUploadText: null,
+            selectedUnit: props.trip.units
         };
+    }
+
+    UNSAFE_componentWillReceiveProps(nextProps, nextContext) {
+        this.setState({selectedUnit: nextProps.trip.units});
     }
 
     render() {
@@ -28,28 +38,130 @@ export default class TripSettingsModal extends React.Component {
 
     renderBody() {
         return (
-            <ModalBody>
-                {this.renderInputRow(
-                    <InputGroup>
-                        <InputGroupAddon addonType="prepend">Trip Title</InputGroupAddon>
-                        <Input id="settings-title-input" placeholder={this.props.trip.title} value={this.state.titleInput}
-                               onChange={(e) => this.setState({titleInput: e.target.value})} />
-                    </InputGroup>)}
-                {this.renderInputRow(
-                    <InputGroup>
-                        <InputGroupAddon addonType="prepend">Unit</InputGroupAddon>
-                        <Input type="select">
-                            <option>Miles (Earth radius = 3959.0)</option>
-                        </Input>
-                    </InputGroup>)}
-                {this.renderInputRow(
-                    <>
-                        {this.renderLoadButton("load-co-brews", "Load CO-Brews", coBrews)}
-                        {this.renderLoadButton("load-us-brews", "Load US-Brews", usBrews)}
-                        {this.renderLoadButton("load-world-brews", "Load World-Brews", worldBrews)}
-                    </>
-                )}
+            <ModalBody className="ml-3 mr-3">
+                {this.renderTitleInput()}
+                {this.renderUnitSelector()}
+                {this.renderFileUpload()}
+                {this.renderSave()}
+                {this.renderLoadButtons()}
+                {this.renderClearTripButton()}
             </ModalBody>
+        );
+    }
+
+    renderTitleInput() {
+        return (
+            <div className="mt-1 mb-4">
+                <Label for="settings-title-input">Title</Label>
+                <Input id="settings-title-input" placeholder={this.props.trip.title} value={this.state.titleInput}
+                       onChange={(e) => this.setState({titleInput: e.target.value})} />
+            </div>
+        );
+    }
+
+    renderUnitSelector() {
+        const unitOptions = Object.keys(EARTH_RADIUS_UNITS_DEFAULT);
+        return (
+            <Row className="mb-4">
+                <Col>
+                    <Label for="unit-selector">Units</Label>
+                        <Input id="unit-selector" value={this.state.selectedUnit} type="select"
+                               onChange={(e) => this.setState({selectedUnit: e.target.value})}>
+                            {unitOptions.map(item => {
+                                const itemName = capitalize(item);
+                                return <option key={item} value={item}>{itemName}</option>
+                            })}
+                        </Input>
+                </Col>
+                <Col>
+                    <Label for="radius-display">Earth Radius</Label>
+                    <Input id="radius-display" value={EARTH_RADIUS_UNITS_DEFAULT[this.state.selectedUnit]} disabled/>
+                </Col>
+            </Row>
+        );
+    }
+
+    renderSave() {
+        return (
+            <div>
+                <Label for="download-button">Save Trip</Label>
+                <Button id="download-button" className="mb-4 float-right" size="sm" color="primary" outline block
+                        onClick={() => this.props.trip.downloadAsJson()}>
+                    Download Trip as JSON
+                </Button>
+            </div>
+        );
+    }
+
+    renderFileUpload() {
+        return (
+            <div className="mb-4">
+                <InputGroup>
+                    <Label for="upload-trip-file">Upload Trip File</Label>
+                    <Input id="upload-trip-file" type="file" accept=".json"
+                           onChange={event => {
+                               const loadedFiles = event.target.files;
+                               if (loadedFiles.length > 0) this.processFile(loadedFiles[0]); }}
+                    />
+                </InputGroup>
+                {this.state.invalidUploadText &&
+                <FormFeedback className="d-block">Error: {this.state.invalidUploadText}</FormFeedback>}
+            </div>
+        );
+    }
+
+    processFile(file) {
+        LOG.info("File selected: " + file.name);
+
+        const read = new FileReader();
+        read.readAsText(file, "UTF-8");
+        read.onloadend = event => {
+            const contentString = event.target.result;
+            try {
+                const loadedData = JSON.parse(contentString);
+                const newTrip = this.props.trip.loadJSON(loadedData);
+                if (newTrip !== this.props.trip) this.updateTrip(newTrip);
+                else this.setState({invalidUploadText: "File must match TripFile.json schema."});
+            } catch (error) {
+                this.setState({invalidUploadText: "Not a valid JSON file."});
+            }
+        }
+    }
+
+    renderLoadButtons() {
+        return (
+            <>
+                <Label for="load-co-brews">Load Class Datasets</Label>
+                <div className="mt-1 mb-2">
+                    {this.renderLoadButton("load-co-brews", "Colorado Brews", coBrews)}
+                    {this.renderLoadButton("load-us-brews", "United States Brews", usBrews)}
+                </div>
+                <div className="mb-4">
+                    {this.renderLoadButton("load-world-brews", "World Brews", worldBrews)}
+                    {this.renderLoadButton("load-world-peaks", "World Peaks", worldPeaks)}
+                </div>
+            </>
+        );
+    }
+
+    renderLoadButton(id, name, tripFile) {
+        return (
+            <Button id={id} className="mr-2" size="sm" color="secondary" outline
+                    onClick={() => this.updateTrip(this.props.trip.loadJSON(tripFile))}>
+                {name}
+            </Button>
+        );
+    };
+
+    renderClearTripButton() {
+        return (
+            <div className="mt-1">
+                <Label for="clear-trip-button">Delete Trip</Label>
+                <Button id="clear-trip-button" className="mb-2 float-right" size="sm" color="danger" outline block
+                        onClick={() => this.updateTrip(new Trip())}>
+                    Clear Trip Data
+                </Button>
+            </div>
         );
     }
 
@@ -58,8 +170,18 @@ export default class TripSettingsModal extends React.Component {
             <ModalFooter>
                 <Button id="trip-settings-submit" color="primary" type="button"
                         onClick={() => {
-                            if (this.state.titleInput.length > 0)
-                                this.props.setTrip(this.props.trip.setTitle(this.state.titleInput));
+                            const trip = this.props.trip;
+                            const newTitle = this.state.titleInput;
+                            const newUnitName = this.state.selectedUnit;
+                            const newUnitValue = EARTH_RADIUS_UNITS_DEFAULT[this.state.selectedUnit].toString();
+                            const titleChanged = newTitle.length > 0;
+                            const unitChanged = trip.units !== newUnitName || trip.earthRadius !== newUnitValue;
+
+                            let newTrip = trip;
+                            if (titleChanged) newTrip = newTrip.setTitle(newTitle);
+                            if (unitChanged) newTrip = newTrip.setUnits(newUnitName, newUnitValue);
+
+                            if (newTrip !== trip) this.props.setTrip(newTrip);
                             this.resetState();
                         }}>
                     Save Settings
@@ -69,31 +191,14 @@ export default class TripSettingsModal extends React.Component {
         );
     }
 
-    renderInputRow(input) {
-        return (
-            <Row className="ml-2 mr-2 mt-3 mb-4">
-                <Col>
-                    {input}
-                </Col>
-            </Row>
-        );
-    };
-
-    renderLoadButton(id, name, tripFile) {
-        return (
-            <Button id={id} className="mr-3 mb-3" size="sm" color="primary" type="button" outline onClick={() => {
-                const newTrip = this.props.trip.loadJSON(tripFile);
-                this.props.setTrip(newTrip);
-                this.props.updatePlaceData();
-                this.resetState()
-            }}>
-                {name}
-            </Button>
-        );
-    };
+    updateTrip(newTrip) {
+        this.props.setTrip(newTrip);
+        this.props.updatePlaceData();
+        this.resetState()
+    }
 
     resetState() {
-        this.setState({titleInput: ""});
+        this.setState({titleInput: "", invalidUploadText: null});
         this.props.toggleOpen();
     };
 }
